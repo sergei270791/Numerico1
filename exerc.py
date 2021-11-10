c09bacc9439ae9075e10907cc80a5ea724d59fc8
sergei270791 <sergei.calle.c@uni.pe>
2021-11-10 02:06:49 -0500

    PD4-Pro

--- exerc.py
+++ exerc.py
@@ -24,26 +24,16 @@ def gradienteConjugado(A,B,tol):
         #print (x[:,1])
         k+=1
 
-def matrizHilbert(n):
-    A=np.zeros((n, n))
-    B=np.zeros((n,1))
-    for i in range(0,n):
-        aux=0
-        for j in range(0,n):
-            A[i,j]=1/(3+i+j)
-            aux+=A[i,j]
-            B[i,0]=(1/3)*aux
-    return A,B
-
 def matriz(n):
     A=np.zeros((n, n))
     for i in range(n):
         for j in range(n):
             if j==i:
-                A[i,j]=2*i
-            elif j==i+1 or j==i-1 :
-                [i,j]=-1
+                A[i,j]=4
+            elif (j==i+1 and i%3!=2) or (j==i-1 and j%3!=2) or j==i+3 or j==i-3  :
+                A[i,j]=-1
     return A
+
 
 def matriz2(n):
     A=np.zeros((n, n))
@@ -51,35 +41,18 @@ def matriz2(n):
         for j in range(n):
             if j==i:
                 A[i,j]=4
-            elif (j==i+1 and i%5!=4) or (j==i-1 and j%5!=4) or j==i+5 or j==i-5 :
+            elif (j==i+1 and i%4!=3) or (j==i-1 and j%4!=3) or j==i+4 or j==i-4  :
                 A[i,j]=-1
     return A
 
-#tol = np.finfo(float).eps
-""" tol =1.0e-5
-A=matriz(40)
-b=np.array([1.5*i-6 for i in range(1,41)])
-b=np.transpose(b)
+""" A=matriz(6)
+b=np.transpose(np.array([0,5,0,6,-2,6]))
+tol=1E-6
 gradienteConjugado(A,b,tol) """
 
-
-
-tol =1.0e-5
-A=matriz2(25)
-b=np.array([1, 0, -1, 0, 2, 1, 0, -1, 0, 2, 1, 0, -1, 0, 2, 1, 0, -1, 0, 2, 1, 0, -1, 0, 2])
-b=np.transpose(b)
-gradienteConjugado(A,b,tol)
-
-
-""" b = np.array([1.902207, 1.051143, 1.175689, 3.480083, 0.819600, -0.264419, -0.412789, 1.175689, 0.913337
+tol=1E-5
+b = np.array([1.902207, 1.051143, 1.175689, 3.480083, 0.819600, -0.264419, -0.412789, 1.175689, 0.913337
 , -0.150209, -0.264419, 1.051143, 1.966694, 0.913337, 0.819600, 1.902207])
 b=np.transpose(b)
 A=matriz2(16)
 gradienteConjugado(A,b,tol)
-"""
-
-
-""" for i in range(2,10):
-        print("Para la matriz de hilbert de orden "+str(i)+" el desarrollo es: ")
-        A=matrizHilbert(i)
-        gradienteConjugado(A,B,tol)  """
