6cfca08dc25bb0a2e418cee4691e7ecfc42fd442
sergei270791 <sergei.calle.c@uni.pe>
2022-07-07 16:46:55 -0500

    mejora givens

--- MetodoGivens.py
+++ MetodoGivens.py
@@ -1,7 +1,7 @@
 from math import copysign, hypot
 
 import numpy as np
-
+#Este es mas recomendado usar que el otro de givens
 
 def _givens_rotation_matrix_entries(a, b):
     """Compute matrix entries for Givens rotation."""
@@ -13,34 +13,25 @@ def _givens_rotation_matrix_entries(a, b):
 
 
 def givens_rotation(A):
-
     (num_rows, num_cols) = np.shape(A)
-
     # Initialize orthogonal matrix Q and upper triangular matrix R.
     Q = np.identity(num_rows)
     R = np.copy(A)
-
     # Iterate over lower triangular matrix.
     (rows, cols) = np.tril_indices(num_rows, -1, num_cols)
-    
     for (row, col) in zip(rows, cols):
-
         # Compute Givens rotation matrix and
         # zero-out lower triangular matrix entries.
         if R[row, col] != 0:
             (c, s) = _givens_rotation_matrix_entries(R[col, col], R[row, col])
-
             G = np.identity(num_rows)
             G[[col, row], [col, row]] = c
             G[row, col] = s
             G[col, row] = -s
-
             R = np.dot(G, R)
             Q = np.dot(Q, G.T)
             print("---------------------")
             print(G)
-            
-	
     return (Q, R)
 
 
@@ -49,12 +40,10 @@ def givens_rotation(A):
 np.set_printoptions(precision=4, suppress=True)
 
 # Input matrix
-A=np.array([
-    [4, 1, 1, 1],
-    [1, 4, 1, 1],
-    [1 ,1 ,4 ,1],
-    [1 ,1 ,1 ,4]],float)
-
+A=np.matrix([
+    [2, 1,1],
+    [1, 2,1],
+    [1 ,1,1 ]],float)
 # Print input matrix
 #print(A)
 
