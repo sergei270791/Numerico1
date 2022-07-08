71d1ba81a5a533646f74ab28a07ec1075d64e160
sergei270791 <sergei.calle.c@uni.pe>
2022-07-07 21:38:05 -0500

    mejora secante

--- MetodoReglaFalsa.py
+++ MetodoReglaFalsa.py
@@ -7,17 +7,17 @@ def ReglaFalsa(f, a, b, tol=1.0e-6):
       raise ValueError("La función debe cambiar de signo en el intervalo")
     if tol <= 0:
       raise ValueError("La cota de error debe ser un número positivo")
-    i=0
-    x=0
+    i=1
+    x=a-f(a)*(a-b)/(f(a)-f(b))
     while abs(f(x)) > tol and abs(x-b) > tol and i<100:
-      x = a-f(a)*(a-b)/(f(a)-f(b))
-      i+=1
       if f(x)==0:
         a=b=x
       elif np.sign(f(x))*np.sign(f(a))<0:
         b=x
       else: 
         a=x
+      x = a-f(a)*(a-b)/(f(a)-f(b))
+      i+=1
     print("Solucion aproximada por Regla falsa: ",x)
     print("El numero de iteraciones es: ",i)
 
--- MetodoSecante.py
+++ MetodoSecante.py
@@ -1,21 +1,21 @@
-from math import log
+from math import log,sin
 def secante(f, a, b, tol=1.0e-6):
+  if a > b:
+    raise ValueError("Intervalo mal definido")
+  if f(a) * f(b) >= 0:
+    raise ValueError("La función debe cambiar de signo en el intervalo")
   if tol <= 0:
     raise ValueError("La cota de error debe ser un número positivo")
-  #x0=0
-  x0=0.4
-  x = (a + b) / 2.0
+  x0=a
+  x=b
   i=0
-  while abs(x-x0) >= tol:
+  while abs(f(x)) > tol and i<100:
       aux=x0
-      x0 = x
+      x0=x
       x = x0-f(x0)*(x0-aux)/(f(x0)-f(aux))
       i+=1
   print("El número de iteraciones es: ",i)
   return x
 
-def funcion(x):
-  return x+log(x)
-
-
-print("La solucion es: ",secante(funcion, 0.5,1,1.0e-4))
+f= lambda x: x*sin(x)-1
+print("La solucion es: ",secante(f,1,2,1.0e-5))
