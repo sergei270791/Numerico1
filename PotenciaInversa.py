e40db3be828f0aa9d607fd92dc3bf26f61df2278
sergei270791 <sergei.calle.c@uni.pe>
2022-07-30 22:21:49 -0500

    mejora de Potencia Inversa

--- PotenciaInversa.py
+++ PotenciaInversa.py
@@ -1,33 +1,31 @@
 import numpy as np
-import sys
+
 def PotenciaInversa(A,x,o,tol):
-  k=0
-  aux=0
-  aux2=0
-  aux4=0
-  while abs(aux4)>tol or aux==0:
-    k=k+1
-    if k==3:
-      aux=1
-    aux3=aux2
-    x=np.dot(np.linalg.inv(A),x)
-    aux2=np.amax(x)
-    x=x/aux2
-    aux4=abs(aux2-aux3)
-  aux2=o+1/aux2
-  aux2=round(aux2,6)
-  print("El autovalor dominante es {}".format(aux2))
-  print("El numero de iteraciones es {}".format(k))
-  x=np.around(x, decimals=6)
-  return np.transpose(x)
+    k=0
+    aux2=0
+    while True:
+        k+=1
+        aux=x
+        x=np.dot(np.linalg.inv(A),x)
+        aux2=np.amax(abs(x))
+        if aux2  not in x:
+            aux2=-aux2
+        x=x/aux2
+        aux2=o+1/aux2
+        print('k=',k,'  x_',k,'=',x.T,' r_',k,'=',aux2)
+        if np.linalg.norm(x-aux)<tol:
+            break
+    print("El autovalor dominante es {}".format(aux2))
+    print("El numero de iteraciones es {}".format(k))
+    return np.transpose(x)
 
-A = np.array([[3.0, -1.0, 0.0],
-                [-1.0, 2.0, -1.0],
-                [0.0, -1.0, 3.0]],float)
+A = np.array([[-4, 14, 0],
+                [-5, 13, 0],
+                [-1, 0 ,2]],float)
 
-x=np.transpose(np.matrix([5.0,1.0,1.0]))
-o=2.8
+x=np.transpose(np.matrix([1.0,1.0,1.0]))
+o=(float)(np.dot(x.T,np.dot(A,x))/(np.dot(x.T,x)))
 A=A-o*np.identity(3)
 
-x=PotenciaInversa(A,x,o,1.0e-7)
+x=PotenciaInversa(A,x,o,1.0e-5)
 print(x)
