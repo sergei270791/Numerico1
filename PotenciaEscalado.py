b70f40fdc623ada41ad15a4bfd1ef5fb7428f3da
sergei270791 <sergei.calle.c@uni.pe>
2022-07-30 19:51:15 -0500

    mejora Potencia Escalado

--- PotenciaEscalado.py
+++ PotenciaEscalado.py
@@ -1,23 +1,26 @@
 import numpy as np
 
-def PotenciaEscalada(A,x,tol):
-  k=0
-  aux=0
-  aux2=np.amax(x)
-  while abs(aux2-aux)>=tol:
-    aux=aux2
-    k=k+1
-    x=np.dot(A,x)
-    aux2=np.amax(x)
-    x=x/aux2
-  
-  print("El autovalor dominante es {}".format(aux2))
-  print("El numero de iteraciones es {}".format(k))
-  return np.transpose(x)
+np.set_printoptions(suppress=True)
 
-x = np.array([[1,0,0]]).T
-A = np.array([[1, 3, -2],
-            [-1, -2, 3],
-            [1, 1, 2]])
-x=PotenciaEscalada(A,x,1.0e-19)
+def PotenciaEscalada(A,x,tol):
+    k=0
+    aux=np.array([[0,0,0]]).T
+    aux2=np.amax(x)
+    print('k=',k,'  x_',k,'=',x.T)
+    while np.linalg.norm(x-aux,np.inf)>=tol:
+        aux=x
+        k=k+1
+        x=np.dot(A,x)
+        aux2=np.amax(abs(x))
+        x=x/aux2
+        print('k=',k,'  x_',k,'=',x.T,' r_',k,'=',aux2)
+    print("El autovalor dominante es ",aux2)
+    print("El numero de iteraciones es ",k)
+    return np.transpose(x)
+
+x = np.array([[1,1,0]]).T
+A = np.array([[2, 0, 2],
+            [-1, 2, 1],
+            [0, 1, 4]])
+x=PotenciaEscalada(A,x,1.0e-12)
 print(x)
